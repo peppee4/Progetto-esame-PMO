f7524be87eafc8203a8595f01d042146470560fe
peppee4 <g.giuliani22@campus.uniurb.it>
2025-02-26 19:12:08 +0100

    Creata la matrice da usare come labirinto

--- Game.java
+++ Game.java
@@ -1,3 +1,6 @@
 public class Game {
-    
+    public static void main(String[] args) {
+        Maze m = new Maze();
+        m.printMaze();
+    }
 }
--- Maze.java
+++ Maze.java
@@ -1,3 +1,29 @@
 public class Maze {
-    
+    // Definizione della matrice del labirinto
+    private int maze[][] = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    // Metodo per stampare il labirinto
+    public void printMaze() {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j] == 1) {
+                    System.out.print("# "); // Muro
+                } else {
+                    System.out.print(". "); // Spazio vuoto
+                }
+            }
+            System.out.println();
+        }
+    }
 }
