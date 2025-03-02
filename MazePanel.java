5e5497c39812af81efa494e76cff10fead51a6cf
paolofania01 <paolofania01@gmail.com>
2025-03-02 17:33:27 +0100

    Update MazePanel.java

--- MazePanel.java
+++ MazePanel.java
@@ -1,3 +1,13 @@
-public class MazePanel {
+
+import javax.swing.*;
+
+public class MazePanel extends JPanel{
     
+    private Maze maze;
+    private Player player;
+
+    public MazePanel (Maze m, Player p){
+        this.maze = m;
+        this.player = p;
+    }
 }
